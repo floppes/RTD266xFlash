fddcdd4e8ae0e910802c741ea34aa7818a3d6ed2
Florian Geib <floppe@floppes.com>
2019-01-23 20:55:23 +0100

    Version incremented

--- RTD266xFlash/RTD266xFlash/Properties/AssemblyInfo.cs
+++ RTD266xFlash/RTD266xFlash/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RTD2660Flash")]
-[assembly: AssemblyCopyright("Copyright © 2018 floppes")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 floppes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
