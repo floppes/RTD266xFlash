d8f69b86560e46978c156f24e177d2415f5f32a4
Asherah Connor <ashe@kivikakk.ee>
2021-02-07 12:14:56 +1100

    write, verify

--- RTD266xFlash/RTD266xFlash/flash.rb
+++ RTD266xFlash/RTD266xFlash/flash.rb
@@ -53,6 +53,33 @@ class RTD266xArduino
     resp[2..-1].pack('C*')
   end
 
+  # Erases the sector _containing_ address. Sectors are probably 4096/0x1000 bytes.
+  def erase_sector_containing(address)
+    write_command([CMD[:ERASE_SECTOR], (address >> 16) & 0xFF, (address >> 8) & 0xFF, address & 0xFF])
+    resp = read_response(2, timeout: 5)
+    raise "mismatched reply" if resp[0] != CMD[:ERASE_SECTOR]
+    raise "res #{resp[1]}" if resp[1] != RES[:OK]
+    nil
+  end
+
+  def write_data(address, data)
+    if data.length > 256
+      raise "looks like i2c can only deal with 256 bytes at a time"
+    end
+
+    write_command([
+      CMD[:WRITE],
+      (address >> 16) & 0xFF, (address >> 8) & 0xFF, address & 0xFF,
+      (data.length >> 8) & 0xFF, data.length & 0xFF,
+      *data.bytes,
+    ])
+
+    resp = read_response(2, timeout: 1)
+    raise "mismatched reply" if resp[0] != CMD[:WRITE]
+    raise "res #{resp[1]}" if resp[1] != RES[:OK]
+    nil
+  end
+
   private
 
   def write_command(cmd)
@@ -106,22 +133,73 @@ end
 
 puts "connected!"
 
-puts "dumping 4MiB of flash in 512 byte increments"
-f = File.open("flash-contents.bin", "wb")
-addr = 0
-top = 4 * 1024 * 1024
-start = Time.now
-while addr < top
+cmd = ARGV.shift
+
+def spinner(addr, top, start, code: nil, length: 80)
+  xs = addr * length / top
+  spaces = length - xs
   print "["
-  print("X" * (addr * 80 / top))
-  print(" " * ((top - addr) * 80 / top))
-  print "] #{"/-\\|"[(Time.now - start).to_i % 4]} #{addr.to_s.reverse.scan(/\d{3}|.+/).join(",").reverse.rjust(9)} \r"
+  print("X" * xs)
+  print(" " * spaces)
+  print "] #{code ? "#{code} " : ""}#{"/-\\|"[(Time.now - start).to_i % 4]} #{addr.to_s.reverse.scan(/\d{3}|.+/).join(",").reverse.rjust(9)} \r"
   STDOUT.flush
-
-  segment = arduino.read_segment(addr, 512)
-  f.write(segment)
-  addr += 512
 end
-f.close
-  f.write(segment)
 
+if cmd == "dump"
+  puts "dumping 4MiB of flash in 512 byte increments"
+  f = File.open("flash-contents.bin", "wb")
+  addr = 0
+  top = 4 * 1024 * 1024
+  start = Time.now
+  while addr < top
+    spinner(addr, top, start)
+    segment = arduino.read_segment(addr, 512)
+    f.write(segment)
+    addr += 512
+  end
+  spinner(addr, top, start)
+  print "\n"
+  f.close
+
+elsif cmd == "write"
+  file = ARGV.shift
+  raise "specify a filename" unless file
+
+  content = File.open(file, "rb", &:read)
+  puts "writing #{content.length} bytes to flash in 256 byte increments"
+
+  addr = 0
+  start = Time.now
+  while addr < content.length
+    if addr % 4096 == 0
+      spinner(addr, content.length, start, code: 'E')
+      arduino.erase_sector_containing(addr)
+    end
+    spinner(addr, content.length, start, code: 'W')
+    arduino.write_data(addr, content[addr...addr+256])
+    addr += 256
+  end
+  spinner(addr, content.length, start)
+  print "\n"
+
+elsif cmd == "verify"
+  file = ARGV.shift
+  raise "specify a filename" unless file
+
+  content = File.open(file, "rb", &:read)
+  puts "verifying #{content.length} bytes in flash in 512 byte"
+
+  addr = 0
+  start = Time.now
+  while addr < content.length
+    spinner(addr, content.length, start)
+    segment = arduino.read_segment(addr, 512)
+    if segment != content[addr...addr+512]
+      print "\n"
+      puts "verify error in bytes #{addr}...#{addr+512}"
+    end
+    addr += 512
+  end
+  spinner(addr, content.length, start)
+  print "\n"
+end
